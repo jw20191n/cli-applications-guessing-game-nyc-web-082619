64ea739d0fe4ed19d28f6d1beefd71ff6e4431eb
Jiayi Wang <clairejw1015@gmail.com>
2019-08-24 18:19:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,12 @@ def run_guessing_game
   random = rand(6) + 1
   puts "Guess a number between 1 to 6."
   answer = gets.chomp
+  
+  if answer == random 
+    puts "You Guessed the correct number!"
+  elsif answer == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{random}"
+  end
 end
